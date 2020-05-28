3d672d0ddc8aa934475b2e2076f3ddb3088e258b
sowmiya2127 <59552127+sowmiya2127@users.noreply.github.com>
2020-05-28 13:38:02 +0530

    Create odd.java

---
+++ mettl/day4/odd/odd.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode
+{
+ public class isOdd(input1){
+      d=input1;
+      if(d%2!=0)
+      return 2;
+      else
+      return 1;
+     }
+    }
