d0af11d438332bc02531a2184116b2e7716ad859
sowmiya2127 <59552127+sowmiya2127@users.noreply.github.com>
2020-05-29 17:48:36 +0530

    Create day5_part2_b_balancedarray.java

---
+++ hackerrank/day5_part2_b_balancedarray.java
@@ -0,0 +1,25 @@
+public static int balancedSum(List<Integer> arr) {
+    // Write your code here
+        int leftSum = 0;
+        int totalSum = 0;
+        
+        int i = 0;
+        int size = arr.size();
+        while(i < size) {
+            totalSum += arr.get(i);
+            i++;
+        }
+        
+        i = 0;
+        while(i < size) {
+            if(totalSum - arr.get(i) == leftSum) {
+                return i;
+            } else {
+                leftSum += arr.get(i);
+                totalSum -= arr.get(i);
+            }
+            i++;
+        }
+        
+        return -1;
+    }
