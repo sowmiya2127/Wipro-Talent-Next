0b3c5ddcc582f2c96c10cefa0d8c3e9aa3c3bc2c
sowmiya2127 <59552127+sowmiya2127@users.noreply.github.com>
2020-06-02 15:04:19 +0530

    Create day8_a.java

---
+++ hackerrank/day8/day8_a.java
@@ -0,0 +1,11 @@
+int N = s.length(), ans = 0;
+        for (int center = 0; center <= 2*N-1; ++center) {
+            int left = center / 2;
+            int right = left + center % 2;
+            while (left >= 0 && right < N && s.charAt(left) == s.charAt(right)) {
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
