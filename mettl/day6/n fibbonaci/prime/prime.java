cf3a54abad9ec1ca1965d59a260d8550207fe539
sowmiya2127 <59552127+sowmiya2127@users.noreply.github.com>
2020-06-06 10:37:44 +0530

    Update prime.java

--- mettl/day6/n fibbonaci/prime/prime.java
+++ mettl/day6/n fibbonaci/prime/prime.java
@@ -1,22 +1,15 @@
-int n=input1;
-for (i = 2; i <= n / 2; ++i) {
-
-        // condition for non-prime
-        if (n % i == 0) {
-            flag = 1;
-            break;
-        }
-    }
-
-    if (n == 1) {
-        return 2;
-    }
-    else {
-        if (flag == 0)
-            return 2;
-        else
-           return 1;
-    }
-
-    return 0;
+import java.io.*;
+import.java.util.*;
+class UserMainCode{
+public int isPrime(int input1){
+int count=0;
+for(int i=1;i<=input1;i++){
+if(input1%i==0)
+count++;
+}
+if(count==2)
+return 2;
+else
+return 1;
+}
 }
