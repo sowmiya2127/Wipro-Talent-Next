97cf84613fe0dae388ef9ce6ab776fc09f88c9c7
sowmiya2127 <59552127+sowmiya2127@users.noreply.github.com>
2020-06-06 10:34:20 +0530

    Update nth prime.java

--- mettl/day6/n fibbonaci/nthprime/nth prime.java
+++ mettl/day6/n fibbonaci/nthprime/nth prime.java
@@ -1,17 +1,15 @@
-int count=0;
-		int no=1,i;
-	while(count<input1)
-			{
-				no+=1;
-				for( i=2;i<=no;i++)
-				{
-					if(no%i==0)
-					break;
-				}
-				if(i==no)
-				count+=1;
-			}
-			return no;
-		
-	}
+int k=2,d=0,i,c=0,p=0;
+while(d<=input1){
+for(i=2;i<k/2;i++){
+if(k%i==0){
+c++;
 }
+}
+if(c==0){
+d++;
+p=k;
+}
+k++;
+c=0;
+}
+return p;
