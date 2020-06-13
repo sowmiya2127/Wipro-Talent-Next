3d3d21be6692fc0451598d2e89fc331d51b6d0c5
sowmiya2127 <59552127+sowmiya2127@users.noreply.github.com>
2020-06-13 20:42:56 +0530

    Delete day12_a.java

--- hackerrank/day12/day12_a.java
+++
@@ -1,8 +0,0 @@
-String ss;
-    char[] wo=word.toCharArray();
-    int len=wo.length;
-    char lt=wo[len-2];
-    char ltb=wo[len-1];
-    ss = ltb+" "+lt;
-     Arrays.toString(ss.toCharArray());
-     return ss;
